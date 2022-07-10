a22ad48a773c3d79e7ce3bed0d4000f149732c52
bumpy09 <101124139+bumpy09@users.noreply.github.com>
2022-07-10 17:45:40 +0800

    new project!

--- main.ts
+++ main.ts
@@ -1,1 +1,67 @@
-
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        . . # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # # #
+        . . # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+led.plotBarGraph(
+1000,
+1000
+)
